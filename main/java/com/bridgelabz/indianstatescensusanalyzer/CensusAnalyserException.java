fcaf77992cbe554a6368cd32aec558224deeabcb
unknown <papisettynagendra>
2022-09-09 17:57:26 +0530

    Added: Indian State code class

--- main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyserException.java
+++ main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyserException.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.indianstatescensusanalyzer;
 
 public class CensusAnalyserException extends Exception {
+
     enum ExceptionType {
         CENSUS_FILE_PROBLEM, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER
     }
@@ -17,5 +18,3 @@ public class CensusAnalyserException extends Exception {
         this.type = type;
     }
 }
-
-
